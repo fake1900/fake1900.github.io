0b400651fad25fbc280f3c8d92a9dc23e0ca3eee
huangpeixin <hi.peixin@qq.com>
2022-07-02 17:22:43 +0800

    init

--- common.js
+++ common.js
@@ -1,12 +1,18 @@
 console.info('hello~~~~~~~~~~~~')
 
+// 记录跳转历史
 let routes = JSON.parse(localStorage.getItem('routes')||'[]')
-let index = routes.indexOf(window.location.href)
-if(index!=-1) {
-    routes.length = index + 1
-} else {
-    routes.push(window.location.href)
+let href = window.location.href
+
+function pushRoute() {
+    let index = routes.indexOf(href)
+    if(index!=-1) {
+        routes.length = index + 1
+    } else {
+        routes.push(window.location.href)
+    }
+    localStorage.setItem('routes', JSON.stringify(routes))
 }
-localStorage.setItem('routes', JSON.stringify(routes))
+pushRoute()
     
 

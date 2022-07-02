81e2e14bb07b9f7eac8f03e087d94686dd5f9aab
huangpeixin <hi.peixin@qq.com>
2022-07-02 18:38:36 +0800

    init

--- common.js
+++ common.js
@@ -14,5 +14,33 @@ function pushRoute() {
     localStorage.setItem('routes', JSON.stringify(routes))
 }
 pushRoute()
-    
+
+ 
+function addBottomBar() {
+    let div = document.querySelector('#bottom-bar')
+    if(!div) {
+        div = document.createElement("div");
+        div.id = 'bottom-bar'
+        div.style = 'font-size: 20px; display: flex; justify-content: space-between; align-items: center; max-width: 46.25rem; height: 5em; margin: 0 auto; padding: 2em; box-sizing: border-box; font-weight: bold; color: #fa8919;'
+        div.innerHTML = '<a onclick="back()">返回</a><a onclick="next()">下一篇</a>'
+        document.body.appendChild(div);
+    }
+}
+// 返回
+function back() {
+    routes.pop()
+    localStorage.setItem('routes', JSON.stringify(routes))
+    console.info(routes[routes.length-1])
+    location.href = routes[routes.length-1]
+}
+// 下一篇
+function next() {
+    alert('开发中...')
+}
+ 
+window.onscroll = function() {
+ if (window.innerHeight + window.pageYOffset >= document.body.offsetHeight) {
+    addBottomBar()
+ }
+}
 
